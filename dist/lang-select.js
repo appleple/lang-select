d24734cf9a6be98306e91857c81bdb88f5381193
Yoshi0207 <yoaplid002@hotmail.co.jp>
2020-07-04 20:49:22 +0900

    細かい修正

--- dist/lang-select.js
+++ dist/lang-select.js
@@ -1,40 +1,46 @@
 "use strict";
 var LangSelect = /** @class */ (function () {
+    // コンストラクタメソッド
     function LangSelect(id, info_list) {
-        this.id = id;
-        this.info_list = info_list;
-        if (this.chack_browser_cookiy() == false) { // ブラウザの第一言語がサイトの対象言語と一致しておらず、かつ、ブラウザにクッキーが埋め込まれていない
-            var prop = this.chack_info_list(); // info_listの言語が一致するプロパティを取り出す
-            if (prop != null) {
-                this.recommend_site_change(prop); // サイトの変更を提案するprivateメソッド
+        this.id = id; // id格納
+        this.info_list = info_list; // Map配列格納
+        if (this.chack_browser_cookiy() == false) { // ブラウザに前回拒否したことを示すクッキーが埋め込まれていない
+            var prop = this.chack_info_list(); // info_listからユーザの第一言語と一致するプロパティを取り出す
+            if (prop != null) { // nullで無ければ
+                this.recommend_site_change(prop); // サイトの変更を提案するHTMLを表示する
             }
         }
     }
-    // サイトの変更を提案するhtmlを表示するメソッド
+    // サイトの変更を提案するHTMLを表示するメソッド
     LangSelect.prototype.recommend_site_change = function (prop) {
         var _this = this;
-        var div_lang_select = document.getElementById(this.id);
-        div_lang_select === null || div_lang_select === void 0 ? void 0 : div_lang_select.classList.add("active");
+        var div_lang_select = document.getElementById(this.id); // id名で要素を抽出
+        div_lang_select === null || div_lang_select === void 0 ? void 0 : div_lang_select.classList.add("active"); // 抽出したdivにactive属性を追加
+        // HTML挿入
         div_lang_select === null || div_lang_select === void 0 ? void 0 : div_lang_select.insertAdjacentHTML("afterbegin", "<div class=\"message\">\n    <p>" + prop["message"] + "</p>\n</div>\n<div class=\"change-site\">\n    <a href=" + prop["url"] + ">" + prop["btn_message"] + "</a>\n</div>\n<div class=\"reject-message\">\n    <button>\u00D7</button>\n</div>");
-        var div_change_site = div_lang_select === null || div_lang_select === void 0 ? void 0 : div_lang_select.getElementsByTagName("div")[2];
-        div_change_site === null || div_change_site === void 0 ? void 0 : div_change_site.addEventListener('click', function () { _this.reject_recomend_event(); });
+        var div_change_site = div_lang_select === null || div_lang_select === void 0 ? void 0 : div_lang_select.getElementsByTagName("div")[2]; // reject-messageのDOMを抽出
+        div_change_site === null || div_change_site === void 0 ? void 0 : div_change_site.addEventListener('click', function () { _this.reject_recomend_event(); }); // clickイベント this.reject_recomend_eventを追加
     };
     // ブラウザの第一言語が一致するプロパティがあるかどうかを探索する
     // 一致するプロパティがあった場合はそのプロパティを、無い場合はnullを返す
     LangSelect.prototype.chack_info_list = function () {
-        for (var i = 0; i < this.info_list.length; i++) {
-            if (this.info_list[i]["lang"] == navigator.language) {
-                return this.info_list[i];
+        console.log(navigator.language.split("-")[0]);
+        for (var _i = 0, _a = this.info_list; _i < _a.length; _i++) {
+            var info = _a[_i];
+            if (info["lang"] == navigator.language.split("-")[0]) {
+                return info;
             }
         }
         return null;
     };
-    // ブラウザのクッキーの有無を取得するメソッド
+    // ブラウザのクッキーの有無を確認するメソッド
+    // クッキー(LangSelectRejectRecomend)があればtrue，無ければfalse
     LangSelect.prototype.chack_browser_cookiy = function () {
-        var cookie = document.cookie;
-        var cookies = cookie.split(';');
-        for (var i = 0; i < cookies.length; i++) {
-            if (cookies[i] == " LangSelectRejectRecomend=true") { // Lの前に空白があるが、それはブラウザにより異なるのだろうか？正規表示した方が塩梅？
+        var cookie_list = document.cookie; // 保存されているクッキー読み出し
+        var cookies = cookie_list.split(';'); // ;で分割し配列cookiesに格納
+        for (var _i = 0, cookies_1 = cookies; _i < cookies_1.length; _i++) {
+            var cookie = cookies_1[_i];
+            if (cookie.replace(/\s+/g, "") == "LangSelectRejectRecomend=true") {
                 return true;
             }
         }
@@ -51,13 +57,14 @@ var LangSelect = /** @class */ (function () {
     LangSelect.prototype.remove_lang_select = function () {
         var div_lang_select = document.getElementById(this.id);
         div_lang_select === null || div_lang_select === void 0 ? void 0 : div_lang_select.classList.remove("active");
+        // div class="lang-select"内の子要素を全て削除
         while (div_lang_select === null || div_lang_select === void 0 ? void 0 : div_lang_select.firstChild)
             div_lang_select.removeChild(div_lang_select.firstChild);
     };
-    // 提案の消去ボタンが押されたときの処理を行うメソッド
+    // 提案の消去ボタンが押されたとき呼び出されるメソッド
     LangSelect.prototype.reject_recomend_event = function () {
-        this.embedding_cookiy();
-        this.remove_lang_select();
+        this.embedding_cookiy(); // クッキーを埋め込む
+        this.remove_lang_select(); // 提案HTMLを削除
     };
     return LangSelect;
 }());
--- src/lang-select.ts
+++ src/lang-select.ts
@@ -1,22 +1,33 @@
-class LangSelect {
-    private id: string;
-    private info_list: {lang: string, url: string, message: string, btn_message: string}[];    
+// 型Lang_InfoはユーザがLangSelectインスタンス生成時に渡すMapの型
+type Lang_Info ={
+    lang: string,
+    url: string,
+    message: string,
+    btn_message: string
+}
 
-    public constructor(id: string, info_list: {lang: string, url: string, message: string, btn_message: string}[]) {
-        this.id = id;
-        this.info_list = info_list;
-        if (this.chack_browser_cookiy()==false) {  // ブラウザの第一言語がサイトの対象言語と一致しておらず、かつ、ブラウザにクッキーが埋め込まれていない
-            const prop: {lang: string, url: string, message: string, btn_message: string} | null = this.chack_info_list();  // info_listの言語が一致するプロパティを取り出す
-            if (prop!=null) {
-                this.recommend_site_change(prop);  // サイトの変更を提案するprivateメソッド
+class LangSelect {
+    private id: string;               // 生成されたインスタンスが対象とするDOMのidを格納する変数
+    private info_list: Lang_Info[];   // インスタンス生成時に渡されたMapの配列を格納する変数
+
+    // コンストラクタメソッド
+    public constructor(id: string, info_list: Lang_Info[]) {
+        this.id = id;               // id格納
+        this.info_list = info_list; // Map配列格納
+        if (this.chack_browser_cookiy()==false) {                   // ブラウザに前回拒否したことを示すクッキーが埋め込まれていない
+            const prop: Lang_Info | null = this.chack_info_list();  // info_listからユーザの第一言語と一致するプロパティを取り出す
+            if (prop!=null) {                      // nullで無ければ
+                this.recommend_site_change(prop);  // サイトの変更を提案するHTMLを表示する
             }
         }
     }
 
-    // サイトの変更を提案するhtmlを表示するメソッド
-    private recommend_site_change(prop: {lang: string, url: string, message: string, btn_message: string}): void {
-        const div_lang_select: HTMLElement | null = document.getElementById(this.id);
-        div_lang_select?.classList.add("active");
+    // サイトの変更を提案するHTMLを表示するメソッド
+    private recommend_site_change(prop: Lang_Info): void {
+        const div_lang_select: HTMLElement | null = document.getElementById(this.id);  // id名で要素を抽出
+        div_lang_select?.classList.add("active");   // 抽出したdivにactive属性を追加
+
+        // HTML挿入
         div_lang_select?.insertAdjacentHTML("afterbegin", 
 `<div class=\"message\">
     <p>${prop["message"]}</p>
@@ -27,28 +38,31 @@ class LangSelect {
 <div class=\"reject-message\">
     <button>×</button>
 </div>`);
-        const div_change_site: HTMLDivElement | undefined = div_lang_select?.getElementsByTagName("div")[2];
-        div_change_site?.addEventListener('click', ()=>{this.reject_recomend_event()});
+
+        const div_change_site: HTMLDivElement | undefined = div_lang_select?.getElementsByTagName("div")[2]; // reject-messageのDOMを抽出
+        div_change_site?.addEventListener('click', ()=>{this.reject_recomend_event()}); // clickイベント this.reject_recomend_eventを追加
     }   
 
     // ブラウザの第一言語が一致するプロパティがあるかどうかを探索する
     // 一致するプロパティがあった場合はそのプロパティを、無い場合はnullを返す
-    private chack_info_list(): {lang: string, url: string, message: string, btn_message: string} | null {
-        for (let i: number = 0; i<this.info_list.length; i++) {
-            if (this.info_list[i]["lang"]==navigator.language) {
-                return this.info_list[i];
+    private chack_info_list(): Lang_Info | null {
+        for (let info of this.info_list) {
+            // navigator.language.split("-")[0]は言語コードと国コードが連結されている場合に言語コードのみを取り出す処理
+            if (info["lang"]==navigator.language.split("-")[0]) {
+                return info;
             }
         }
         return null;
     }
 
-    // ブラウザのクッキーの有無を取得するメソッド
+    // ブラウザのクッキーの有無を確認するメソッド
+    // クッキー(LangSelectRejectRecomend)があればtrue，無ければfalse
     private chack_browser_cookiy(): boolean {
-        const cookie: string = document.cookie;
-        const cookies: string[] = cookie.split(';');
+        const cookie_list: string = document.cookie;      // 保存されているクッキー読み出し
+        const cookies: string[] = cookie_list.split(';'); // ;で分割し配列cookiesに格納
 
-        for (let i: number = 0; i<cookies.length; i++) {
-            if (cookies[i]==" LangSelectRejectRecomend=true") { // Lの前に空白があるが、それはブラウザにより異なるのだろうか？正規表示した方が塩梅？
+        for (let cookie of cookies) {
+            if (cookie.replace(/\s+/g, "")=="LangSelectRejectRecomend=true") {
                 return true;
             }
         }
@@ -67,12 +81,14 @@ class LangSelect {
     private remove_lang_select(): void {
         const div_lang_select: HTMLElement | null = document.getElementById(this.id);
         div_lang_select?.classList.remove("active");
+
+        // div class="lang-select"内の子要素を全て削除
         while (div_lang_select?.firstChild) div_lang_select.removeChild(div_lang_select.firstChild);
     }
 
-    // 提案の消去ボタンが押されたときの処理を行うメソッド
+    // 提案の消去ボタンが押されたとき呼び出されるメソッド
     public reject_recomend_event(): void {
-        this.embedding_cookiy();
-        this.remove_lang_select();
+        this.embedding_cookiy();   // クッキーを埋め込む
+        this.remove_lang_select(); // 提案HTMLを削除
     }
 }
